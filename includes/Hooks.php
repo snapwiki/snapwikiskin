026fa6cb15bbb5ea979745e84eade73db80eb4db
GrahamSH <grahamshllk@gmail.com>
2021-05-10 16:05:34 -0400

    Change stuffs

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -1,26 +1,11 @@
 <?php
 use ResourceLoader;
+class Hooks {
 public static function onResourceLoaderRegisterModules( ResourceLoader $rl ) {
-		if ( $rl->isModuleRegistered( 'wvui' ) ) {
-			$module = [
-				'localBasePath' => __DIR__ . '/..',
-				'remoteExtPath' => 'snapwikiskin',
-				'dependencies' => [
-					'mediawiki.util',
-				],
-				"packageFiles" => [
-					"resources/skins.snapwikiskin.search/skins.snapwikiskin.search.js",
-					"resources/skins.snapwikiskin.search/App.vue"
-				],
-				"dependencies" => [
-					"wvui"
-				],
-				"messages" => [
-					"search",
-					"searchresults",
-					"searchsuggest-containing"
-				],
-			];
-			$rl->register( 'skins.snapwikiskin.search', $module );
-		}
+	public static function getVectorWvuiSearchResourceLoaderConfig(
+		ResourceLoaderContext $context,
+		Config $config
+	) {
+		return $config->get( 'VectorWvuiSearchOptions' );
 	}
+}
