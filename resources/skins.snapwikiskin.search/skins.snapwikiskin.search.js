bdc0ba973f362841a3ae0b5cf35fbe23e5f2dc76
R4356th <R4356th@users.noreply.github.com>
2021-05-29 13:34:43 +0000

    Automatically fixing lint errors

--- resources/skins.snapwikiskin.search/skins.snapwikiskin.search.js
+++ resources/skins.snapwikiskin.search/skins.snapwikiskin.search.js
@@ -1,51 +1,51 @@
 /** @module search */
-const Vue = require( 'vue' ).default || require( 'vue' ),
-      App = require( './App.vue' ),
-      config = require( './config.json' );
+const Vue = require("vue").default || require("vue"),
+  App = require("./App.vue"),
+  config = require("./config.json");
 /**
  * @param {HTMLElement} searchForm
  * @param {HTMLInputElement} search
  * @return {void}
  */
 // eslint-disable-next-line no-implicit-globals
-function initApp( searchForm, search ) {
-      // eslint-disable-next-line no-new
-      new Vue( {
-            el: searchForm,
-            // eslint-disable-next-line jsdoc/check-alignment
-            /**
-             *
-             * @param {Function} createElement
-             * @return {Vue.VNode}
-             */
-            render: function ( createElement ) {
-      return createElement( App, {
+function initApp(searchForm, search) {
+  // eslint-disable-next-line no-new
+  new Vue({
+    el: searchForm,
+    // eslint-disable-next-line jsdoc/check-alignment
+    /**
+     *
+     * @param {Function} createElement
+     * @return {Vue.VNode}
+     */
+    render: function (createElement) {
+      return createElement(App, {
         props: $.extend(
           {
             autofocusInput: search === document.activeElement,
-            action: searchForm.getAttribute( 'action' ),
-            searchAccessKey: search.getAttribute( 'accessKey' ),
-            searchTitle: search.getAttribute( 'title' ),
-            searchPlaceholder: search.getAttribute( 'placeholder' ),
+            action: searchForm.getAttribute("action"),
+            searchAccessKey: search.getAttribute("accessKey"),
+            searchTitle: search.getAttribute("title"),
+            searchPlaceholder: search.getAttribute("placeholder"),
             searchQuery: search.value,
           },
           // Pass additional config from server.
           config
-         ),
-       } );
-     },
-   } );
- }
+        ),
+      });
+    },
+  });
+}
 /**
  * @param {Document} document
  * @return {void}
  */
 // eslint-disable-next-line no-implicit-globals
-function main( document ) {
-  const searchForm = /** @type {HTMLElement} */ ( document.querySelector( '#searchform' ) ),
-    search = /** @type {HTMLInputElement|null} */ ( document.getElementById( 'searchInput' ) );
-  if ( search && searchForm ) {
-    initApp( searchForm, search );
+function main(document) {
+  const searchForm = /** @type {HTMLElement} */ (document.querySelector("#searchform")),
+    search = /** @type {HTMLInputElement|null} */ (document.getElementById("searchInput"));
+  if (search && searchForm) {
+    initApp(searchForm, search);
   }
 }
-main( document );
+main(document);
