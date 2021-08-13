cf42ed5f7b79ba15aa9bbb9eef54b8bbaa6a49b5
R4356th <R4356th@users.noreply.github.com>
2021-08-13 19:05:38 +0000

    style: format code

--- resources/dist/skins.snapwikiskin.search.js
+++ resources/dist/skins.snapwikiskin.search.js
@@ -1,24 +1,30 @@
 "use strict";
-const Vue = require("vue").default || require("vue"), App = require("./App.vue"), config = require("./config.json");
+const Vue = require("vue").default || require("vue"),
+  App = require("./App.vue"),
+  config = require("./config.json");
 function initApp(searchForm, search) {
   new Vue({
     el: searchForm,
-    render: function(createElement) {
+    render: function (createElement) {
       return createElement(App, {
-        props: $.extend({
-          autofocusInput: search === document.activeElement,
-          action: searchForm.getAttribute("action"),
-          searchAccessKey: search.getAttribute("accessKey"),
-          searchTitle: search.getAttribute("title"),
-          searchPlaceholder: search.getAttribute("placeholder"),
-          searchQuery: search.value
-        }, config)
+        props: $.extend(
+          {
+            autofocusInput: search === document.activeElement,
+            action: searchForm.getAttribute("action"),
+            searchAccessKey: search.getAttribute("accessKey"),
+            searchTitle: search.getAttribute("title"),
+            searchPlaceholder: search.getAttribute("placeholder"),
+            searchQuery: search.value,
+          },
+          config
+        ),
       });
-    }
+    },
   });
 }
 function main(document2) {
-  const searchForm = document2.querySelector("#searchform"), search = document2.getElementById("searchInput");
+  const searchForm = document2.querySelector("#searchform"),
+    search = document2.getElementById("searchInput");
   if (search && searchForm) {
     initApp(searchForm, search);
   }
