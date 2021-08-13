b47301dd45b6c89315e2ffe9b76edcfbaabcf6fe
R4356th <R4356th@users.noreply.github.com>
2021-08-13 19:16:38 +0000

    style: format code

--- resources/dist/skins.snapwikiskin.search.js
+++ resources/dist/skins.snapwikiskin.search.js
@@ -1,1 +1,30 @@
-"use strict";const e=require("vue").default||require("vue"),t=require("./App.vue"),r=require("./config.json");!function(c){const u=c.querySelector("#searchform"),n=c.getElementById("searchInput");n&&u&&function(c,u){new e({el:c,render:function(e){return e(t,{props:$.extend({autofocusInput:u===document.activeElement,action:c.getAttribute("action"),searchAccessKey:u.getAttribute("accessKey"),searchTitle:u.getAttribute("title"),searchPlaceholder:u.getAttribute("placeholder"),searchQuery:u.value},r)})}})}(u,n)}(document);
+"use strict";
+const e = require("vue").default || require("vue"),
+  t = require("./App.vue"),
+  r = require("./config.json");
+!(function (c) {
+  const u = c.querySelector("#searchform"),
+    n = c.getElementById("searchInput");
+  n &&
+    u &&
+    (function (c, u) {
+      new e({
+        el: c,
+        render: function (e) {
+          return e(t, {
+            props: $.extend(
+              {
+                autofocusInput: u === document.activeElement,
+                action: c.getAttribute("action"),
+                searchAccessKey: u.getAttribute("accessKey"),
+                searchTitle: u.getAttribute("title"),
+                searchPlaceholder: u.getAttribute("placeholder"),
+                searchQuery: u.value,
+              },
+              r
+            ),
+          });
+        },
+      });
+    })(u, n);
+})(document);
